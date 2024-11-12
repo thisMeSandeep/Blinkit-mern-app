5a11c93a10239d2bda3e8d15e24e866889e429c5
Sandeep Singh <snayal50@gmail.com>
2024-11-12 21:57:58 +0530

    login route

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -2,11 +2,14 @@ import express from "express";
 import {
   registerUserController,
   verifyEmailController,
+  loginController,
 } from "../controllers/user.controller.js";
 
 const userRouter = express.Router();
 
 userRouter.route("/register").post(registerUserController); // for registering user
-userRouter.route("/verify-email/:code").post(verifyEmailController);
+userRouter.route("/verify-email/:code").post(verifyEmailController); //for verifying email
+userRouter.route("/login").post(loginController);  // for login 
 
 export default userRouter;
+ 
